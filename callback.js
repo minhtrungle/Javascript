b3f19de0e0fd35d52576c3229d38b6c9b440e4b0
Your Name <you@example.com>
2022-03-23 21:17:09 +0700

    callback

--- callback.js
+++ callback.js
@@ -3,27 +3,61 @@
 function myFunction(param) {
     param(123456);
 }
-//CallBack
+
 myFunction(myCallback);
 
-function myCallback1() {
-    console.log(1);
+//CallBack
+function thuchien() {
+    console.log('_____');
 }
-function myCallback2() {
-    console.log(2);
+function viec(doCallback) {
+    doCallback();
 }
-myCallback1();
-myCallback2();
+viec(thuchien);
 
 //Callback Hell
 function callBackhell1(temp){
     temp();
 }
 
-function callBackhell(temp){
+function callBackhell2(temp){
     temp();
 }
 
 function callBackhell3(temp){
     temp();
 }
+
+//map
+Array.prototype.mapA = function (callback) {
+    let output =[];
+    arrayLength = this.length;
+
+    for (let i = 0; i < arrayLength; i++) {
+        let result = callback(this[i], i);
+        output.push(result);
+    }
+
+    return output;
+    
+};
+
+const courses = ["L", "M", "T"];
+
+const temp = courses.mapA(function (element) {
+    console.log(element);
+});
+
+//Foreach, find, fillter
+//foreach2
+Array.prototype.forEach2 = function (callback) {
+    for (let index in this) {
+        if (this.hasOwnProperty(index)) {
+            callback(this[index], index, this);
+        }
+    }
+};
+
+courses.forEach2(function (course, index, array) {
+    console.log(course, index, array);
+});
