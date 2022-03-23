a470602a014e539678cdbcca930a6329ab87ce83
Your Name <you@example.com>
2022-03-23 22:19:46 +0700

    callback/callback hell

--- callback.js
+++ callback.js
@@ -1,10 +1,4 @@
 // 1. Là một hàm
-// 2. 
-function myFunction(param) {
-    param(123456);
-}
-
-myFunction(myCallback);
 
 //CallBack
 function thuchien() {
@@ -28,36 +22,4 @@ function callBackhell3(temp){
     temp();
 }
 
-//map
-Array.prototype.mapA = function (callback) {
-    let output =[];
-    arrayLength = this.length;
 
-    for (let i = 0; i < arrayLength; i++) {
-        let result = callback(this[i], i);
-        output.push(result);
-    }
-
-    return output;
-    
-};
-
-const courses = ["L", "M", "T"];
-
-const temp = courses.mapA(function (element) {
-    console.log(element);
-});
-
-//Foreach, find, fillter
-//foreach2
-Array.prototype.forEach2 = function (callback) {
-    for (let index in this) {
-        if (this.hasOwnProperty(index)) {
-            callback(this[index], index, this);
-        }
-    }
-};
-
-courses.forEach2(function (course, index, array) {
-    console.log(course, index, array);
-});
